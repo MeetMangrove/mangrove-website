7b39044177c20906ee6c6d94903962c1033dab08
AmbroiseCollon <ambroise.collon@gmail.com>
2016-11-02 01:03:55 +0100

    Added wording from google sheet

---
+++ middleware/wording.js
@@ -0,0 +1,161 @@
+var key = require('./client_secret.json');
+var google = require('googleapis');
+var sheets = google.sheets('v4')
+
+//
+var auth = new google.auth.JWT(
+  key.client_email,
+  null,
+  key.private_key,
+  ['https://www.googleapis.com/auth/spreadsheets.readonly', 'https://www.googleapis.com/auth/drive'],
+  null
+);
+
+/*var drive = google.drive('v3');
+drive.files.list({
+	auth: auth
+}, function (err, resp) {
+	console.log(err);
+});*/
+
+var pages = {
+	home: {
+		ranges: ['Main!A2:E', 'Home!A2:D'],
+		index: 0
+	},
+	values: {
+		ranges: ['Main!A2:E', 'Values!A2:E'],
+		index: 1
+	},
+	lifestyle: {
+		ranges: ['Main!A2:E', 'Lifestyle!A2:C'],
+		index: 2
+	},
+	community: {
+		ranges: ['Main!A2:E', 'Community!A2:C'],
+		index: 3
+	},
+	team: {
+		ranges: ['Main!A2:E'],
+		index: 4
+	}
+}
+
+function getWording(name) {
+	return new Promise(function (resolve, reject) {
+		// Make auth
+		auth.authorize(function (err, tokens) {
+			// Auth error
+			if (err) {
+				reject(err)
+				return
+			}
+
+			// Get page
+			var page = pages[name]
+			if (!page) {
+				reject('Page name does not exists. Possible values are : home, values, lifestyle, community, team')
+				return
+			}
+
+			sheets.spreadsheets.values.batchGet({
+				auth: auth,
+				spreadsheetId: '1eCOYdNHmHyPEktZbQP5D6vbQtCbGRs059NyoL26XzXI',
+				ranges: page.ranges
+			}, function(err, response) {
+				// Error handler
+				if (err) {
+					reject(err)
+					return;
+				}
+
+				// Fill generic info
+				var tabs = response.valueRanges
+				var rows = tabs[0].values
+				var row = rows[page.index]
+				var wording = {
+					title: row[1],
+					catchphrase: row[2],
+					cta: row[3],
+					description: row[4]
+				}
+
+				if (name != 'team') {
+					// Fill page specific information
+					rows = tabs[1].values
+
+					switch (name) {
+					case 'home': // Home
+						wording.sections = []
+						for (var j = 0; j < rows.length; j++) {
+							var row = rows[j]
+							var section = {
+								title: row[0],
+								text: row[1],
+								cta: row[2],
+								cta_link: row[3]
+							}
+							wording.sections.push(section)
+						}
+						break;
+					case 'values': // Values
+						var row = rows[0]
+						var section = {
+							title: row[0],
+							text: row[1],
+							cta: row[2],
+							cta_link: row[3]
+						}
+						wording.section = section
+						wording.manifesto = row[4]
+						break;
+					case 'lifestyle': // Lifestyle
+						wording.sections = []
+						for (var j = 0; j < rows.length; j++) {
+							var row = rows[j]
+							var section = {
+								title: row[0],
+								text: row[1],
+								image_detail: row[2]
+							}
+							wording.sections.push(section)
+						}
+						break;
+					case 'community': // Community
+						wording.sections = []
+						for (var j = 0; j < rows.length; j++) {
+							var row = rows[j]
+							var section = {
+								title: row[0],
+								text: row[1],
+								image_detail: row[2]
+							}
+							wording.sections.push(section)
+						}
+						break;
+					default:
+						break;
+					}
+				}
+				resolve(wording)
+			});
+		});
+	});
+}
+
+var wordingMiddleware = function (req, res, next) {
+	var path = req.path.substring(1)
+	path = (path == '') ? 'home' : path
+	getWording(path).
+		then(
+			function (wording) {
+				req.wording = wording
+				next()
+			},
+			function (err) {
+				next(err)
+			}
+		)
+};
+
+	module.exports = wordingMiddleware
--- routes/index.js
+++ routes/index.js
@@ -1,70 +1,27 @@
 var express = require('express');
-var router = express.Router();
+var wording = require('../middleware/wording.js')
 
-/* GET home page. */
+var router = express.Router();
+router.use(wording);
+
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Mangrove',
-    catchphrase: 'From Work to Life.',
-    cta: 'Start a new life. Join us now',
-    description: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla',
-    sections: [
-      {
-        title: 'Work differently.',
-        text: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla',
-        cta: { link: 'http://google.com', text: 'Read our manifesto' },
-        background: 'images/fish.jpg'
-      },
-      {
-        title: 'Work differently.',
-        text: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla',
-        cta: { link: 'http://google.com', text: 'Read our manifesto' },
-        background: 'images/fish.jpg'
-      },
-      {
-        title: 'Work differently.',
-        text: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla',
-        cta: { link: 'http://google.com', text: 'Read our manifesto' },
-        background: 'images/fish.jpg'
-      }
-    ]
-  });
+  res.render('index', req.wording)
 });
 
 router.get('/values', function(req, res, next) {
-  res.render('values', {
-    title: 'Mangrove',
-    catchphrase: 'From Work to Life.',
-    cta: 'Start a new life. Join us now',
-    description: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla',
-  })
+  res.render('values', req.wording)
 });
 
 router.get('/projects', function(req, res, next) {
-  res.render('values', {
-    title: 'Mangrove',
-    catchphrase: 'Our community is our strength',
-    cta: 'Start a new life. Join us now',
-    description: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla',
-  })
+  res.render('values', req.wording)
 });
 
 router.get('/lifestyle', function(req, res, next) {
-  res.render('values', {
-    title: 'Mangrove',
-    catchphrase: 'Hubs, Retreats & Bootcamps.',
-    cta: 'Start a new life. Join us now',
-    description: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla',
-  })
+  res.render('values', req.wording)
 });
 
 router.get('/community', function(req, res, next) {
-  res.render('values', {
-    title: 'Mangrove',
-    catchphrase: 'Co-founders and friends',
-    cta: 'Start a new life. Join us now',
-    description: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla',
-  })
+  res.render('values', req.wording)
 });
 
 module.exports = router;
