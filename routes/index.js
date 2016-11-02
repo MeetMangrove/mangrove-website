46e482069e4638c9ee605c10fd26ef6e10e9ddee
AmbroiseCollon <ambroise.collon@gmail.com>
2016-11-02 01:21:25 +0100

    OMG only one router !!!

--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,10 @@ var wording = require('../middleware/wording.js')
 var router = express.Router();
 router.use(wording);
 
-router.get('/', function(req, res, next) {
-  res.render('index', req.wording)
-});
-
-router.get('/values', function(req, res, next) {
-  res.render('values', req.wording)
-});
-
-router.get('/lifestyle', function(req, res, next) {
-  res.render('lifestyle', req.wording)
-});
-
-router.get('/community', function(req, res, next) {
-  res.render('community', req.wording)
-});
-
-router.get('/team', function(req, res, next) {
-  res.render('team', req.wording)
+router.get('/:name?', function(req, res, next) {
+	var name = req.params.name
+	name = (name) ? name : 'index'
+	res.render(name, req.wording)
 });
 
 module.exports = router;
