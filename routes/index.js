98cdddb43c0a572501b1f47711896a2f5bc7aee7
Yannis <yannis.chaouche@gmail.com>
2016-11-02 01:06:43 +0100

    Nouveau commit stylé

--- routes/index.js
+++ routes/index.js
@@ -40,13 +40,32 @@ router.get('/values', function(req, res, next) {
   })
 });
 
-router.get('/projects', function(req, res, next) {
-  res.render('values', {
+router.get('/community', function(req, res, next) {
+  res.render('community', {
     title: 'Mangrove',
     catchphrase: 'Our community is our strength',
     cta: 'Start a new life. Join us now',
     description: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla',
-  })
+    sections: [
+      {
+        title: 'Work differently.',
+        text: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla',
+        cta: { link: 'http://google.com', text: 'Read our manifesto' },
+        background: 'images/fish.jpg'
+      },
+      {
+        title: 'Work differently.',
+        text: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla',
+        cta: { link: 'http://google.com', text: 'Read our manifesto' },
+        background: 'images/fish.jpg'
+      },
+      {
+        title: 'Work differently.',
+        text: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla',
+        cta: { link: 'http://google.com', text: 'Read our manifesto' },
+        background: 'images/fish.jpg'
+      }
+    ]  })
 });
 
 router.get('/lifestyle', function(req, res, next) {
@@ -78,7 +97,7 @@ router.get('/lifestyle', function(req, res, next) {
   })
 });
 
-router.get('/community', function(req, res, next) {
+router.get('/team', function(req, res, next) {
   res.render('values', {
     title: 'Mangrove',
     catchphrase: 'Co-founders and friends',
