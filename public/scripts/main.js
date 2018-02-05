e6dc23f2b9327d6281b3dc12760c5a248f4ba754
Nicolas Wielonsky <nicolas.wielonsky@gmail.com>
2018-02-05 15:18:58 +0100

    Hellobar: Woman at Mangrove

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
   $('.bottom-arrow').click(function () {
     $(window).scrollTo($('#head'), 500)
   })
+  $('.hellobar-clear').click(function () {
+    $('.banner').fadeOut('fast')
+  })
 })
