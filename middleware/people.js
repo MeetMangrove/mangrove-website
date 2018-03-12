2da96e5a57926bc983d8f8195a03ff3d8db3151c
Nicolas Wielonsky <nicolas.wielonsky@gmail.com>
2018-03-12 20:49:35 +0100

    Homepage + Fix images

--- middleware/people.js
+++ middleware/people.js
@@ -1,163 +1,163 @@
-var airtable = require('./airtable.js')
+var airtable = require("./airtable.js");
 
-function getPeople () {
-  return new Promise(function (resolve, reject) {
+function getPeople() {
+  return new Promise(function(resolve, reject) {
     var members = [],
       friends = [],
-      locations = []
+      locations = [];
 
     airtable.members
       .select({
         // filterByFormula: "{Status} = 'Cofounder'",
       })
       .eachPage(
-        function page (records, fetchNextPage) {
-          records.forEach(function (record) {
-            var status = record.get('Status')
+        function page(records, fetchNextPage) {
+          records.forEach(function(record) {
+            var status = record.get("Status");
             if (
-              status === 'Builder' ||
-              status === 'Resident' ||
-              status === 'Veteran'
+              status === "Builder" ||
+              status === "Resident" ||
+              status === "Veteran"
             ) {
-              members.push(record)
-            } else if (status === 'Friend' || status === 'Guest') {
-              friends.push(record)
+              members.push(record);
+            } else if (status === "Friend" || status === "Guest") {
+              friends.push(record);
             }
-            locations.push(record.get('Current Location'))
-          })
-          fetchNextPage()
+            locations.push(record.get("Current Location"));
+          });
+          fetchNextPage();
         },
-        function done (err) {
+        function done(err) {
           if (err) {
-            reject(err)
-            return
+            reject(err);
+            return;
           }
 
-          members = sortMembers(members)
-          members = formatMembers(members)
-          friends = formatFriends(friends)
+          members = sortMembers(members);
+          members = formatMembers(members);
+          friends = formatFriends(friends);
 
-          console.log('=> Retrieved ' + members.length + ' members.')
-          console.log('=> Retrieved ' + friends.length + ' friends.')
+          console.log("=> Retrieved " + members.length + " members.");
+          console.log("=> Retrieved " + friends.length + " friends.");
 
           resolve({
             members: members,
             friends: friends,
             locations: locations
-          })
+          });
         }
-      )
-  })
+      );
+  });
 }
 
-function formatFriends (records) {
-  var friends = []
+function formatFriends(records) {
+  var friends = [];
 
   for (var i = 0; i < records.length; i++) {
-    var record = records[i]
+    var record = records[i];
     var tw =
-      record.get('Twitter') && record.get('Twitter').length
-        ? record.get('Twitter')
-        : null
+      record.get("Twitter") && record.get("Twitter").length
+        ? record.get("Twitter")
+        : null;
     var img =
-      record.get('Profile Picture') && record.get('Profile Picture').length
-        ? record.get('Profile Picture')[0].url
-        : null
+      record.get("Profile Picture") && record.get("Profile Picture").length
+        ? record.get("Profile Picture")[0].url
+        : null;
     var person = {
-      name: record.get('Name'),
+      name: record.get("Name"),
       twitter: tw,
       image: img
-    }
+    };
 
-    friends.push(person)
+    friends.push(person);
   }
 
-  return friends
+  return friends;
 }
 
-function formatMembers (records) {
-  var members = []
+function formatMembers(records) {
+  var members = [];
 
-  var threeMonthAgo = new Date()
-  threeMonthAgo.setMonth(threeMonthAgo.getMonth() - 3)
+  var threeMonthAgo = new Date();
+  threeMonthAgo.setMonth(threeMonthAgo.getMonth() - 3);
 
   for (var i = 0; i < records.length; i++) {
-    var record = records[i]
+    var record = records[i];
 
     var tw =
-      record.get('Twitter') && record.get('Twitter').length
-        ? record.get('Twitter')
-        : null
+      record.get("Twitter") && record.get("Twitter").length
+        ? record.get("Twitter")
+        : null;
     var img =
-      record.get('Profile Picture') && record.get('Profile Picture').length
-        ? record.get('Profile Picture')[0].url
-        : null
-    var dateOfArrival = new Date(record.get('Cofounder Since'))
-    var location = record.get('Location')
+      record.get("Profile Picture") && record.get("Profile Picture").length
+        ? record.get("Profile Picture")[0].url
+        : null;
+    var dateOfArrival = new Date(record.get("Member Since"));
+    var location = record.get("Location");
 
     var person = {
-      name: record.get('Name'),
+      name: record.get("Name"),
       twitter: tw,
       image: img,
       dateOfArrival: formatArrivalDate(dateOfArrival),
-      tracks: record.get('Tracks') ? record.get('Tracks') : [],
+      tracks: record.get("Tracks") ? record.get("Tracks") : [],
       fire: i < 3,
       newbie: dateOfArrival > threeMonthAgo,
-      skills: record.get('Skills'),
-      bio: truncateText(record.get('Bio'), 100),
-      bioFull: record.get('Bio'),
-      location: location ? location.replace(/,.*/, '') : null
-    }
+      skills: record.get("Skills"),
+      bio: truncateText(record.get("Bio"), 100),
+      bioFull: record.get("Bio"),
+      location: location ? location.replace(/,.*/, "") : null
+    };
 
-    members.push(person)
+    members.push(person);
   }
 
-  return members
+  return members;
 }
 
-function truncateText (str, length, ending) {
+function truncateText(str, length, ending) {
   if (str) {
     if (length == null) {
-      length = 100
+      length = 100;
     }
     if (ending == null) {
-      ending = '...'
+      ending = "...";
     }
     if (str.length > length) {
-      return str.substring(0, length - ending.length) + ending
+      return str.substring(0, length - ending.length) + ending;
     } else {
-      return str
+      return str;
     }
   }
 }
 
-function sortMembers (members) {
-  members.sort(function (a, b) {
-    return b.get('Last Month Points') - a.get('Last Month Points')
-  })
+function sortMembers(members) {
+  members.sort(function(a, b) {
+    return b.get("Last Month Points") - a.get("Last Month Points");
+  });
 
-  return members
+  return members;
 }
 
-function formatArrivalDate (date) {
+function formatArrivalDate(date) {
   var monthNames = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-  ]
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
 
-  return 'since ' + monthNames[date.getMonth()] + ' ' + date.getFullYear()
+  return monthNames[date.getMonth()] + " " + date.getFullYear();
 }
 
 module.exports = {
   get: getPeople
-}
+};
